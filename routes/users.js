51bd73040c37790954ffa595982fb9e5b1a65bfa
hamzahhamlet <hamzah.hamlet@gmail.com>
2020-01-11 13:38:13 +0500

    user model added

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/User");
+
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
 router.post("/", (req, res) => {
-	res.json({ msg: "Register a user." });
+	// res.json({ msg: "Register a user." });
+	res.send(req.body);
 });
 
 module.exports = router;
